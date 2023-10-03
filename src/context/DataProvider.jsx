da5c01c8d42a2253b8a9e9c0869f8c40cd5f164f
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-10-03 21:09:02 +0530

    main

---
+++ src/context/DataProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, usestate } from "react";
+
+const DataContext = createContext();
+
+const DataProvider =()=>{
+
+    const[html,setHtml] = useState('');
+    const[css,setCss] = useState('');
+    const[js,setJs] = useState('');
+
+    return(
+        <DataContext.Provider
+
+        >
+
+        </DataContext.Provider>
+    )
+}
